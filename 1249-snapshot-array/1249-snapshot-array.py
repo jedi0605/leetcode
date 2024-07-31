7675fc9f21c6f489342f8a6cbe63c1c17e391524
Lee Meng Chieh <jedi2454@gmail.com>
2024-07-31 14:37:31 -0400

    Time: 619 ms (56.58%), Space: 43.2 MB (70.66%) - LeetHub

--- 1249-snapshot-array/1249-snapshot-array.py
+++ 1249-snapshot-array/1249-snapshot-array.py
@@ -1,31 +1,5 @@
 class SnapshotArray:
 
-    # def __init__(self, length: int):
-    #     self.snaps = 0
-    #     self.arr = []
-    #     for i in range(length):
-    #         self.arr.append([])
-
-    # def set(self, index: int, val: int) -> None:
-    #     self.arr[index].append([val, self.snaps])
-
-    # def snap(self) -> int:
-    #     self.snaps += 1
-    #     return self.snaps - 1
-
-    # def get(self, index: int, snap_id: int) -> int:
-    #     values = self.arr[index]
-    #     print(values)
-    #     res = 0
-    #     start, end = 0, len(values) - 1
-    #     while start <= end:
-    #         mid = (start + end) >> 1
-    #         if values[mid][1] <= snap_id:
-    #             res = values[mid][0]
-    #             start = mid + 1
-    #         else:
-    #             end = mid - 1
-    #     return res
     def __init__(self, length: int):
         self.snap_times = 0
         self.arr = []
@@ -36,23 +10,24 @@ class SnapshotArray:
         self.arr[index].append([self.snap_times, val])
 
     def snap(self) -> int:
-        self.snap_times += 1
+        self.snap_times += 1        
+
         return self.snap_times - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        def bSearch(arr, target):
-            l, r = -1, len(arr)
+        def b_search(search_arr, target):
+            l, r = -1, len(search_arr)
             while l + 1 != r:
                 mid = (l + r) // 2
-                if arr[mid][0] <= target:
+                if search_arr[mid][0] <= target:
                     l = mid
                 else:
                     r = mid
-            return l
-
-        datas = self.arr[index]
-        idx = bSearch(datas, snap_id)
-        return self.arr[index][idx][1] if idx != -1 else 0
+            return l if l != -1 else -1
+        search_arr = self.arr[index]
+        idx = b_search(search_arr, snap_id)
+        
+        return search_arr[idx][1] if idx != -1 else 0
 
 
 # Your SnapshotArray object will be instantiated and called as such:
