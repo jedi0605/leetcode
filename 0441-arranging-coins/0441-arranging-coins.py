5233c8cc355118fcc4fd424b8970188d37b0f4b1
Lee Meng Chieh <jedi2454@gmail.com>
2024-08-01 12:59:41 -0400

    Time: 38 ms (77.21%), Space: 16.6 MB (19.25%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,7 +1,20 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        r = 1
-        while (n-r) >= 0:
-            n -= r            
-            r+=1
-        return r - 1
+        # brute force
+        # r = 1
+        # while (n-r) >= 0:
+        #     n -= r
+        #     r+=1
+        # return r - 1
+
+        l, r = 1, n
+        res = 0
+        while l <= r:
+            k = (l + r) // 2
+            space = (k / 2) * (k + 1)
+            if space > n:
+                r = k - 1
+            else:                
+                res = max(res, k)
+                l = k + 1
+        return res
