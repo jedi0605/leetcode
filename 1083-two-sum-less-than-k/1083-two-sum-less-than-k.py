68594a1e826e81dfca4ab2b987a4647e2af70aac
Lee Meng Chieh <jedi2454@gmail.com>
2024-08-06 16:18:13 -0400

    Time: 40 ms (85.42%), Space: 16.6 MB (34.84%) - LeetHub

--- 1083-two-sum-less-than-k/1083-two-sum-less-than-k.py
+++ 1083-two-sum-less-than-k/1083-two-sum-less-than-k.py
@@ -4,15 +4,14 @@ class Solution:
         # if nums[0] + nums[-1] > k:
         #     return -1
         print(nums)
-        l, r = 0 , len(nums)-1
+        l, r = 0, len(nums) - 1
         res = -1
-        while l<r:
+        while l < r:
             if nums[r] > k:
-                r -=1
+                r -= 1
             elif nums[r] + nums[l] < k:
                 res = max(res, nums[r] + nums[l])
-                l+=1
+                l += 1
             else:
-                r-=1
-                l =0
+                r -= 1
         return res
