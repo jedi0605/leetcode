57145f48482763d36d2a5bee23cada0173a16768
Lee Meng Chieh <jedi2454@gmail.com>
2024-08-19 12:15:21 -0400

    Time: 171 ms (28.57%), Space: 18.1 MB (14.58%) - LeetHub

--- 1192-divide-chocolate/1192-divide-chocolate.py
+++ 1192-divide-chocolate/1192-divide-chocolate.py
@@ -12,13 +12,13 @@ class Solution:
 
         l, r = min(sweetness), sum(sweetness)
         ans = 1
-        while l <= r:
+        while l<= r:
             mid = (l + r) // 2
             if canSplit(mid):
                 ans = mid
                 l = mid + 1
             else:
-                r = mid - 1
+                r = mid -1
         return ans
 
         # def dfs(start, remaining_chunks, current_split):
