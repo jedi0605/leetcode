622d3a1cf291424ef98d092f6365fc791cc6e364
Lee Meng Chieh <jedi2454@gmail.com>
2024-08-24 15:25:31 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1929-maximum-value-at-a-given-index-in-a-bounded-array/1929-maximum-value-at-a-given-index-in-a-bounded-array.py
+++ 1929-maximum-value-at-a-given-index-in-a-bounded-array/1929-maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -1,50 +1,45 @@
 class Solution:
     def maxValue(self, n: int, index: int, maxSum: int) -> int:
-        def work2(max_val):
-            mid = max_val -1
-            r = n - index - 1 
-            r_sum = 0
+        def worked(max_num):
+
+            # 1 2 3 4.  3 2 1
+            # .    l mid r
+            m = max_num - 1  # avoid th middle part
             l = index
-            l_sum = 0
-            if r > mid:
-                r_sum = mid*(mid+1) // 2 + r-mid
+            r = n - index - 1
+            lsum, rsum = 0, 0
+            if l > m:
+                lsum = m * (m + 1) // 2 + l - m
             else:
-                r_sum = mid*(mid+1)//2 - (mid-r) *(mid-r+1)//2
-            if l>mid:   
-                l_sum = mid*(mid+1) // 2 + l-mid
+                lsum = m * (m + 1) // 2 - (m - l) * (m - l + 1) // 2
+            if r > m:
+                rsum = m * (m + 1) // 2 + r - m
             else:
-                l_sum = mid*(mid+1)//2 - (mid-l) *(mid-l+1)//2
-            total = max_val+r_sum+l_sum
-            return total <= maxSum
-        # def work(max_val):
-        #     if n == 0:
-        #         return max_val <= maxSum
+                rsum = m * (m + 1) // 2 - (m - r) * (m - r + 1) // 2
+            return lsum+rsum+max_num <=maxSum
+            # right
 
-        #     next_val = max_val
-        #     guess_arr = [1] * n
-        #     guess_arr[index] = next_val
-
-        #     l, r = index,index
-        #     while l > 0 or r < n - 1:
-        #         next_val = next_val - 1 if next_val > 1 else 1
-        #         if l > 0:
-        #             l -= 1
-        #             guess_arr[l] = next_val
-        #         if r != n - 1:
-        #             r += 1
-        #             guess_arr[r] = next_val
-                    
-        #     print(guess_arr)
-        #     return sum(guess_arr) <= maxSum
-
-        l,r = 1,maxSum
-        ans = 0
-        while l<=r:
-            mid = (l+r) // 2
-            if work2(mid):
-                ans = mid
-                l = mid+1
+        # def worked(max_num):
+        #     l,r = index -1 ,index +1
+        #     arr = [1] * n
+        #     arr[index] = max_num
+        #     while l >=0 or r<=n-1:
+        #         max_num -=1
+        #         if l >=0:
+        #             arr[l] = max_num if max_num>=1 else 1
+        #             l -=1
+        #         if r <=n -1:
+        #             arr[r] = max_num if max_num>=1 else 1
+        #             r+=1
+        #     # print(arr)
+        #     return sum(arr) <= maxSum
+        l, r = 0, maxSum
+        res = 0
+        while l <= r:
+            m = (l + r) // 2
+            if worked(m):
+                res = m
+                l = m + 1
             else:
-                r =mid-1
-        
-        return ans
+                r = m - 1
+        return res
