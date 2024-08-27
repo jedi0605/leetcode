c61920c995668d370ab9cb1f03b0eb6eadebc5d0
Lee Meng Chieh <jedi2454@gmail.com>
2024-08-27 18:27:33 -0400

    Time: 402 ms (52.45%), Space: 22.4 MB (24.51%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -1,36 +1,32 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        self.max_of_weights = max(weights)
-
-        def work(cap: int) -> bool:
-            curTotal = 0
-            dayNeeds = 1
+        # def work(cap: int) -> bool:
+        #     curTotal = 0
+        #     dayNeeds = 1
+        #     for w in weights:
+        #         if curTotal + w > cap:
+        #             curTotal = w
+        #             dayNeeds += 1
+        #         else:
+        #             curTotal += w
+        #     return dayNeeds <= days
+        def work(cap:int) -> bool:
+            work_d = 1
+            cur_w = 0
             for w in weights:
-                if curTotal + w > cap:
-                    curTotal = w
-                    dayNeeds += 1
+                if cur_w + w > cap:
+                    work_d+=1
+                    cur_w = w
                 else:
-                    curTotal += w
-            return dayNeeds <= days
-            # tmp = weights[:]
-            # # if cap < self.max_of_weights:
-            # #     return False
-            # for d in range(days):
-            #     cap_tmp = cap
-            #     for i in range(len(tmp)):
-            #         if cap_tmp >= tmp[i]:
-            #             cap_tmp -= tmp[i]
-            #             tmp[i] = 0
-            # return sum(tmp) == 0
-
-        l = self.max_of_weights
-        r = sum(weights)
-
-        while l < r:
-            mid = (l + r) // 2
+                    cur_w+=w
+            return work_d<=days
+        work(7)
+        l,r = max(weights), sum(weights)
+        while l<r:
+            mid = (l+r) // 2
             if work(mid) == False:
-                l = mid + 1
+                l = mid +1
             else:
                 r = mid
-
+        print(f"{l}, {r}")
         return l
