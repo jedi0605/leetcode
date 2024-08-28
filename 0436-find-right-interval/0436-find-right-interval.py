1b44136bc9ce7bdc3d066bb1dfec5e883b14af63
Lee Meng Chieh <jedi2454@gmail.com>
2024-08-28 16:44:56 -0400

    Time: 231 ms (80.64%), Space: 22 MB (23.61%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.py
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -1,25 +1,20 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        def bSearch(arr, target):
-            l, r = -1, len(arr)
-            while l + 1 != r:
-                mid = (l + r) // 2
-                if arr[mid][0] < target:
-                    l = mid
-                else:
-                    r = mid
-            return r if r != len(arr) else -1
-
-        mapping = {}  # start not duplicate
+        idx_map = {}
+        sort_arr = []
         for i in range(len(intervals)):
-            mapping[intervals[i][0]] = i
-        intervals.sort()
-        res = [-1] * len(intervals)
-
+            idx_map[intervals[i][0]] = i # {val, idx}
+            sort_arr.append(intervals[i][0])
+        sort_arr.sort()
+        print(idx_map)
+        print(sort_arr)
+        res = []
         for i in range(len(intervals)):
-            right_idx = bSearch(intervals, intervals[i][1])  # closest right val, not idx
-            print(f"{intervals[i][1]}. res {right_idx}")
-
-            if right_idx != -1:
-                res[mapping[intervals[i][0]]] = mapping[intervals[right_idx][0]]
+            index = bisect_left(sort_arr, intervals[i][1])
+            if index == len(sort_arr):
+                res.append(-1)
+            else:
+                index_val = sort_arr[index]
+                res.append(idx_map[index_val])
         return res
+        
