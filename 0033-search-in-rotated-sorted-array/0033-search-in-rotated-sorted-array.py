90304391f00428a218648b3e99508f5961c7d7ff
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-08 20:25:41 -0400

    Time: 44 ms (50.66%), Space: 17 MB (5.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,37 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def bSearch2(nums, start, end, target):
-            l, r = start - 1, end
-            while l + 1 != r:
-                mid = (l + r) // 2
+        def b_search2(start, end):
+            l,r = start -1 , end
+            while l+1 != r:
+                mid = (l+r)//2
                 if nums[mid] < target:
-                    l = mid
+                    l = mid 
                 else:
                     r = mid
-
-            if r == len(nums):
-                return -1
+            if r == len(nums) : return -1
             return r if nums[r] == target else -1
-
-        def bSearch(nums):
-            l, r = -1, len(nums)
-
-            while l + 1 != r:
-                mid = (l + r) // 2
-                if nums[mid] > nums[l]:
+        #find smallest index
+        def b_search():
+            l,r = -1, len(nums)
+            while l+1 != r:
+                mid = (l+r)//2
+                if nums[mid]> nums[l]:
                     l = mid
                 else:
                     r = mid
             return r
-
-        small_idx = 0
-        if nums[0] > nums[-1]:  # have rotated
-            small_idx = bSearch(nums)
-        start, end = 0, 0
-
-        if nums[small_idx] <= target <= nums[-1]:  # right side
-            start, end = small_idx, len(nums)
+        small_idx = b_search()
+        start,end = 0,0
+        if nums[small_idx]<= target <= nums[-1]:
+            start,end = small_idx,len(nums)
         else:
-            start, end = 0, small_idx
-
-        return bSearch2(nums, start, end, target)
+            start,end = 0,small_idx
+        return b_search2(start, end)
