81e3acaf237c14ed6ad46258a63d092566f764a2
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-08 20:35:24 -0400

    Time: 46 ms (37.96%), Space: 17 MB (31.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,29 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         def b_search2(start, end):
-            l,r = start -1 , end
-            while l+1 != r:
-                mid = (l+r)//2
+            l, r = start - 1, end
+            while l + 1 != r:
+                mid = (l + r) // 2
                 if nums[mid] < target:
-                    l = mid 
-                else:
-                    r = mid
-            if r == len(nums) : return -1
-            return r if nums[r] == target else -1
-        #find smallest index
-        def b_search():
-            l,r = -1, len(nums)
-            while l+1 != r:
-                mid = (l+r)//2
-                if nums[mid]> nums[l]:
                     l = mid
                 else:
                     r = mid
+            if r == len(nums):
+                return -1
+            return r if nums[r] == target else -1
+
+        # find smallest index
+        def b_search():
+            l, r = 0, len(nums) - 1
+            while l < r:
+                mid = (l + r) // 2
+                if nums[mid] > nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid
             return r
+
         small_idx = b_search()
-        start,end = 0,0
-        if nums[small_idx]<= target <= nums[-1]:
-            start,end = small_idx,len(nums)
+        start, end = 0, 0
+        if nums[small_idx] <= target <= nums[-1]:
+            start, end = small_idx, len(nums)
         else:
-            start,end = 0,small_idx
+            start, end = 0, small_idx
         return b_search2(start, end)
