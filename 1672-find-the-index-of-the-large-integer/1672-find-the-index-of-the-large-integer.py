4644e8fc05a29c41251d247c19474c84d9791f4f
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-10 15:36:35 -0400

    Time: 146 ms (22.06%), Space: 58 MB (51.47%) - LeetHub

--- 1672-find-the-index-of-the-large-integer/1672-find-the-index-of-the-large-integer.py
+++ 1672-find-the-index-of-the-large-integer/1672-find-the-index-of-the-large-integer.py
@@ -2,35 +2,35 @@
 # This is ArrayReader's API interface.
 # You should not implement it, or speculate about its implementation
 # """
-# class ArrayReader(object):
-# 	 # Compares the sum of arr[l..r] with the sum of arr[x..y]
-# 	 # return 1 if sum(arr[l..r]) > sum(arr[x..y])
-# 	 # return 0 if sum(arr[l..r]) == sum(arr[x..y])
-# 	 # return -1 if sum(arr[l..r]) < sum(arr[x..y])
+#class ArrayReader(object):
+#	 # Compares the sum of arr[l..r] with the sum of arr[x..y]
+#	 # return 1 if sum(arr[l..r]) > sum(arr[x..y])
+#	 # return 0 if sum(arr[l..r]) == sum(arr[x..y])
+#	 # return -1 if sum(arr[l..r]) < sum(arr[x..y])
 #    def compareSub(self, l: int, r: int, x: int, y: int) -> int:
 #
-# 	 # Returns the length of the array
+#	 # Returns the length of the array
 #    def length(self) -> int:
 #
 
 
 class Solution:
-    def getIndex(self, reader: "ArrayReader") -> int:
-        arr_len = reader.length()
-        print(arr_len)
-        l, r = 0, arr_len - 1
-
-        while l < r:
-            mid = (l + r) // 2
-            if mid - l > r - (mid + 1):
-                l += 1  # make sure left arr, right arr are equal
-            comp = reader.compareSub(l, mid, mid + 1, r)
-            if comp == 0:
-                return l - 1
-
-            if comp == 1:
+    def getIndex(self, reader: 'ArrayReader') -> int:
+        size = reader.length()
+        l,r = 0,size -1
+        while l< r:
+            mid = (l+r) // 2
+            sub_size = (r-l)+1
+            res = 0
+            if sub_size %2 == 0:
+                res = reader.compareSub(l,mid,mid+1,r)
+            else:
+                res = reader.compareSub(l,mid,mid,r)
+            
+            if res == 1:
                 r = mid
-            elif comp == -1:
-                l = mid + 1
-
+            elif res == -1:
+                l = mid+1
+            else:
+                return mid
         return l
