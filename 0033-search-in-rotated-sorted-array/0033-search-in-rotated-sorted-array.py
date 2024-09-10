7d04238e0a294975c34e239f8a109afa91f78cb1
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-10 14:41:26 -0400

    Time: 55 ms (5.34%), Space: 16.6 MB (99.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,32 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         def b_search2(start, end):
-            l, r = start - 1, end
+            l, r = start -1 , end
             while l + 1 != r:
                 mid = (l + r) // 2
                 if nums[mid] < target:
                     l = mid
                 else:
                     r = mid
-            if r == len(nums):
+            if r == end:
                 return -1
             return r if nums[r] == target else -1
 
-        # find smallest index
+        ## find smallest index
         def b_search():
-            l, r = 0, len(nums) - 1
-            while l < r:
+            l, r = 0, len(nums)
+            while l + 1 != r:
                 mid = (l + r) // 2
-                if nums[mid] > nums[r]:
-                    l = mid + 1
+                if nums[mid] > nums[l]:
+                    l = mid
                 else:
                     r = mid
             return r
 
-        small_idx = b_search()
+        print(b_search())
+        idx = b_search()
         start, end = 0, 0
-        if nums[small_idx] <= target <= nums[-1]:
-            start, end = small_idx, len(nums)
+
+        if nums[0] <= target <= nums[idx - 1]:
+            start, end = 0, idx
         else:
-            start, end = 0, small_idx
+            start, end = idx, len(nums)
+        print(f"{start},{end}")
         return b_search2(start, end)
