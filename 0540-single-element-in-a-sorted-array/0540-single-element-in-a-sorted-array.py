dff7bf6f66cd7b51c3ab38c99c04838642f6924b
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-11 18:17:49 -0400

    Time: 147 ms (16.45%), Space: 26.6 MB (90.42%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,26 +1,24 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        # Brute force
-        # for i in range(0, len(nums) - 2, 2):
-        #     if nums[i] != nums[i + 1]:
-        #         return nums[i]
-        # return -1
-        l, r = 0, len(nums) -1
-        while l < r:
-            mid = (l + r) // 2
-            are_even = (r - mid) % 2
+        l,r = 0, len(nums)
 
-            if nums[mid] == nums[mid + 1]:
-                if are_even == 0:
-                    l = mid + 2
-                else:
-                    r = mid -1
+        while l<r:
+            mid = (l+r) // 2
 
-            elif nums[mid] == nums[mid - 1]:
-                if are_even == 0:
-                    r = mid -2
+            if mid+1 <len(nums) and nums[mid] == nums[mid+1]:
+                # choose right or left
+                chooseRight = (len(nums) - mid -1)%2
+                if chooseRight:
+                    r = mid-1
                 else:
-                    l = mid + 1
+                    l = mid+2
+            elif mid -1 >=0 and nums[mid] == nums[mid-1]:
+                chooseRight = (len(nums) - mid -1)%2
+                if chooseRight == 0:
+                    r = mid-2
+                else:
+                    l = mid  +1
             else:
                 return nums[mid]
         return nums[l]
+
