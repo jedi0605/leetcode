7a9e1c9aae3495514ff7dc3b3c599e33c9a99bbf
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-11 17:47:57 -0400

    Time: 118 ms (48.32%), Space: 23.1 MB (99.03%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,9 +1,12 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         if len(citations) == 1: return 0 if citations[0] == 0 else 1
-        h = 0
-        citations.sort()
-        for i in range(len(citations)-1,-1,-1):
-            if citations[i] > h:
-                h+=1
-        return h
+        l,r = -1, len(citations)
+        while l+1!=r:
+            mid = (l+r) // 2
+            if len(citations) - mid > citations[mid]:
+                l = mid
+            else:
+                r= mid
+        print(f"{l},{r}")
+        return len(citations) - r
