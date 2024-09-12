f6f4f50791238fc17a0dc6c457c3accf67ede3c5
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-12 12:41:24 -0400

    Time: 248 ms (26.48%), Space: 18.1 MB (69.15%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,38 +1,29 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        idx = bisect_left(arr, x)
+        print(idx)
 
-        left = bisect_left(arr, x) - 1
-        right = left + 1
-
-        print(f"left:{left} right:{right}")
-
+        l, r = idx - 1, idx
         while k > 0:
-            ### out of bound
-            if left < 0:
-                right += 1
-            elif right == len(arr):
-                left -= 1
-            elif x - arr[left] <= arr[right] - x:
-                left -= 1
+            if l < 0:
+                r += 1
+            elif r == len(arr):
+                l -= 1
+            elif x - arr[l] <= arr[r] - x:
+                l -= 1
             else:
-                right += 1
-            ### in bound
+                r += 1
             k -= 1
-        print(f"left:{left} right:{right}")
-
-        return arr[left + 1 : right]
-
-
+            print(f"{l},{r}")
+        return arr[l + 1 : r]
         # dis_idx = []
-
         # for i in range(len(arr)):
-        #     dis_idx.append( (abs(arr[i] - x),i ))
-
+        #     dis_idx.append([abs(arr[i] - x), i])
         # print(dis_idx)
-        # sorted_data = sorted(dis_idx, key=lambda x: (x[0], x[1]))
-        # print(sorted_data)
+        # dis_idx = sorted(dis_idx, key=lambda x: (x[0], x[1]))
+        # print(dis_idx)
         # res = []
         # for i in range(k):
-        #     res.append(arr[  sorted_data[i][1]])
-        # res.sort()
-        # return res
+        #     res.append(arr[dis_idx[i][1]])
+
+        # return sorted(res)
