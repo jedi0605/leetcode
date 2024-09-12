2ff5d8d9e1185aab38d03af9dc9a6f7b0e35cd77
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-12 18:52:14 -0400

    Time: 481 ms (69.6%), Space: 16.7 MB (51.03%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,43 +1,14 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        nums.sort()
         res = 0
+        nums.sort()
+
         for i in range(len(nums)-1,-1,-1):
-            l,r = 0 , i -1
+            l, r = 0,i-1
             while l<r:
-                if nums[l] + nums[r] > nums[i]:
-                    res+=r-l
+                if nums[l]+nums[r] > nums[i]:
+                    res+= r-l
                     r-=1
                 else:
                     l+=1
         return res
-
-
-        # nums.sort()
-        # res = 0
-        # for i in range(len(nums)-2):
-        #     for j in range(i+1, len(nums)-1):
-        #         for k in range(j+1, len(nums)):
-        #             if nums[i]+nums[j] > nums[k]:
-        #                 res+=1
-        # return res
-
-        # nums.sort()  # Sort in ascending order
-        # self.count = 0
-        # n = len(nums)
-
-        # def dfs(index, sides):
-        #     print(sides)
-        #     if len(sides) == 3:
-        #         if sides[0] + sides[1] > sides[2]:
-        #             self.count += 1
-        #         return
-            
-        #     for i in range(index, n):
-        #         sides.append(nums[i])
-        #         dfs(i + 1, sides)
-        #         sides.pop()
-
-        # dfs(0, [])
-        # return self.count
-
