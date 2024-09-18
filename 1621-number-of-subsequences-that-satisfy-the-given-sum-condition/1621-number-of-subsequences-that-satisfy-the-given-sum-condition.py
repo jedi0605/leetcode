fa0fd2410f4e8b8f664652c681b5d3199cd5b099
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-18 13:40:25 -0400

    Time: 6881 ms (51.3%), Space: 29.4 MB (16.68%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,32 +1,33 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
-        # using two pointer
-        # l start from zero
-        nums.sort()
-        l, r = 0, len(nums) - 1
         res = 0
-        mod = 1_000_000_000 + 7
-        while l <= r:
-            if nums[l] + nums[r] > target:
-                r -= 1
-            else:
-                res += 2 ** (r - l)
-                l += 1
+        mod = 10 ** 9 + 7
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] + nums[i] > target:
+                break
+            sub_target = target - nums[i]
+            idx = bisect_right(nums,sub_target) -1
+            if idx < len(nums):
+                res += 2 ** (idx - i)
         return res % mod
+
+        # brute force. list all Sub Seq
         # res = []
         # nums.sort()
-        # def dfs(idx,tmp):
-        #     res.append(tmp[:])
-        #     for i in range(idx, len(nums)):
-        #         tmp.append(nums[i])
-        #         dfs(i + 1,tmp)
-        #         tmp.pop()
-        # dfs(0,[])
+        # def dfs(start, curr_arr):
+        #     if curr_arr:
+        #         res.append(curr_arr[:])
+            
+        #     for i in range(start, len(nums)):
+        #         curr_arr.append(nums[i])
+        #         dfs(i + 1, curr_arr)
+        #         curr_arr.pop()
+
+        # dfs(0, [])
         # print(res)
-        # cnt = 0
-        # for sub_arr in res:
-        #     if len(sub_arr) == 0:
-        #         continue
-        #     if sub_arr[0] + sub_arr[-1] <=target:
-        #         cnt+=1
-        # return cnt
+        # ans = 0
+        # for r in res:
+        #     if r[0]+r[-1] <=target:
+        #         ans+=1        
+        # return ans
