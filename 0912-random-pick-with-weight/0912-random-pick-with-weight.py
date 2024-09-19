9d8fd3c3c72bf661134e92c3606f559c3c35714a
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-19 11:11:18 -0400

    Time: 126 ms (94.44%), Space: 21.5 MB (15.72%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.py
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.py
@@ -1,36 +1,15 @@
 class Solution:
-    # prefix sum
+
     def __init__(self, w: List[int]):
-        # [1,3,2] => using prefix sum [1,4,6] => random pick nums => using bs seach to find the idx
-        self.total = 0
-        self.arr = []
-        for wei in range(len(w)):
-            self.total += w[wei]
-            self.arr.append(self.total)
-
+        self.w_arr = []
+        self.sum = 0
+        for idx,weight in enumerate(w):
+            self.sum+=weight
+            self.w_arr.append(self.sum)
+        print(self.w_arr)  
     def pickIndex(self) -> int:
-        # self.total
-        num = random.randint(1, self.total)
-        l, r = -1, len(self.arr)
-        while l + 1 != r:
-            mid = (l + r) // 2
-            if self.arr[mid] < num:
-                l = mid
-            else:
-                r = mid
-        return 0 if r == len(self.arr) else r
-        # return bisect_left(self.arr, num)
-
-    # # brute force
-    # def __init__(self, w: List[int]):
-    #     # [1,3] => [1,2,2,2] means index 1 have 1weight, index 2 have 3 weight
-    #     self.arr = []
-    #     for wei in range(len(w)):
-    #         for _ in range(w[wei]):
-    #             self.arr.append(wei)
-
-    # def pickIndex(self) -> int:
-    #     return random.choice(self.arr)
+        num =  random.randint(1, self.sum)
+        return bisect.bisect_left(self.w_arr, num)
 
 
 # Your Solution object will be instantiated and called as such:
