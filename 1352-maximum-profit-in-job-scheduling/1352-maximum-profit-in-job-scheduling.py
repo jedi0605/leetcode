0bb4b008a90504fbb608f7e40aab274951bc36d2
Lee Meng Chieh <jedi2454@gmail.com>
2024-09-24 16:12:22 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -1,32 +1,21 @@
 class Solution:
-    def jobScheduling(
-        self, startTime: List[int], endTime: List[int], profit: List[int]
-    ) -> int:
-        
-        jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
-        endArr = [e for s,e,p in jobs]
-        dp = [0] * len(jobs)
-        dp[0] = jobs[0][2] # default profit
-        
-        for i in range(1,len(jobs)):
-            s = jobs[i][0]
-            e = jobs[i][1]
-            p = jobs[i][2]
-            # print(f"{s}.{e}.{p}")
-            idx = bisect_right(endArr, s) -1 # find the last s in endArr
-            # print(idx)
-            dp[i] = max(dp[i-1], (dp[idx] if idx>=0 else 0) +p)
-            # print(dp)
-        return dp[-1]
-        # jobs =sorted( zip(startTime,endTime,profit))
-        # heap = []
-        # maxProfit = 0
-        # for s,e,p in jobs:
-
-        #     while heap and heap[0][0]<=s:
-        #         start, profit = heappop(heap)
-        #         maxProfit = max(profit,maxProfit)
-        #     heappush(heap, (e,p+maxProfit) )
-        # for e,p in heap:
-        #     maxProfit = max(p,maxProfit)
-        # return maxProfit
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        # jobs = zip(startTime,endTime,profit)
+        jobs = []
+        heap = []
+        for i in range(len(startTime)):
+            jobs.append( [startTime[i],endTime[i],profit[i]] )
+        jobs.sort()
+        print(jobs)
+        cur_p = 0
+        max_p = 0
+        for job in jobs:
+            s,e,p = job[0],job[1],job[2]
+            while heap and s >= heap[0][0]:
+                end, cur_p = heapq.heappop(heap)
+                max_p = max(max_p,cur_p)
+            heapq.heappush(heap, [e, p + cur_p])
+        print(heap)
+        for e,p in heap:
+            max_p = max(max_p,p)
+        return max_p
