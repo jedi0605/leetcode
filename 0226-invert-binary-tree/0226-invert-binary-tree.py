835627fa5c0516bb9545b1ba053db2b9a35cd3d7
Lee Meng Chieh <jedi2454@gmail.com>
2024-10-01 12:13:16 -0400

    Time: 34 ms (66.84%), Space: 16.5 MB (66.59%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -10,18 +10,19 @@ class Solution:
         def dfs(root: Optional[TreeNode]):
             if not root:
                 return
-            if root.left == None and root.right == None:
-                return
-
-            if root.left == None and root.right:
-                root.left = root.right
-                root.right = None
-
-            elif root.left and root.right == None:
-                root.right = root.left
-                root.left = None
-            else:
-                root.left, root.right = root.right, root.left
+            tmp = root.left
+            root.left = root.right
+            root.right = tmp
+            # if root.left == None and root.right == None:
+            #     return
+            # if root.left == None and root.right:
+            #     root.left = root.right
+            #     root.right = None
+            # elif root.left and root.right == None:
+            #     root.right = root.left
+            #     root.left = None
+            # else:
+            #     root.left, root.right = root.right, root.left
             dfs(root.left)
             dfs(root.right)
 
