5ba7d62401bf8b66a1ca6292d6f1f25f7f9845d3
Lee Meng Chieh <jedi2454@gmail.com>
2024-10-01 12:17:16 -0400

    Time: 61 ms (8.32%), Space: 17 MB (51.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        cnt = Counter(s)
+        print(cnt)
+        for cht in t:
+            if cht not in cnt or cnt[cht] == 0:
+                return False
+            cnt[cht] -=1
+            
+        return sum(cnt.values()) == 0
