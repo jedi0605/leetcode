c45c16e087571db4210bc8a2b743dd1763dadc5e
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2024-10-03 12:59:19 -0400

    Time: 71 ms (27.34%), Space: 17 MB (6.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -3,9 +3,8 @@ class Solution:
         self, image: List[List[int]], sr: int, sc: int, color: int
     ) -> List[List[int]]:
         visit = set()
-        direction = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         start_c = image[sr][sc]
-        # visist.add((sr,sc))
+
         def dfs(r, c):
             print(f"{r},{c}")
             if (
@@ -21,8 +20,8 @@ class Solution:
             dfs(r - 1, c)
             dfs(r, c + 1)
             dfs(r, c - 1)
+        
         dfs(sr, sc)
-        print(visit)
         for r, c in visit:
             image[r][c] = color
 
