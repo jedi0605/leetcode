ca43edb18f724f3a722b4c624a1caef07a40c11e
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2024-10-09 22:15:17 -0400

    Time: 756 ms (14.06%), Space: 19.8 MB (31.24%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,21 +1,16 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        dp = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
         max_q = 0
+        print(dp)        
 
-        for i in range(len(matrix[0])):
-            dp[0][i] = int(matrix[0][i])
-            max_q = max(max_q, dp[0][i])
-
-        for i in range(len(matrix)):
-            dp[i][0] = int(matrix[i][0])
-            max_q = max(max_q, dp[i][0])
-
-        for r in range(1, len(matrix)):
-            for c in range(1, len(matrix[0])):
-                if matrix[r][c] != "0":
-                    dp[r][c] = 1 + min(dp[r - 1][c - 1], dp[r][c - 1], dp[r - 1][c])
-                    max_q = max(dp[r][c], max_q)
-        print(dp)
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == "1":
+                    if r == 0 or c == 0:
+                        dp[r][c] = 1
+                    else:
+                        dp[r][c] = 1 + min(dp[r - 1][c - 1], dp[r][c - 1], dp[r - 1][c])
+                    max_q = max(dp[r][c], max_q)        
 
         return max_q * max_q
