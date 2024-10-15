029608df94688ba5693a3e3d75ee955e85dc794b
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2024-10-15 17:01:25 -0400

    Time: 70 ms (34%), Space: 16.8 MB (25.07%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,27 +2,33 @@ class Solution:
     def floodFill(
         self, image: List[List[int]], sr: int, sc: int, color: int
     ) -> List[List[int]]:
-        visit = set()
-        start_c = image[sr][sc]
+        queue = deque()
+        visited = set()
+        start_color = image[sr][sc]
+        queue.append((sr, sc))
+        dir_arr = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        while queue:
+            c_r, c_c = queue.popleft()
+            if (c_r, c_c) in visited:
+                continue
 
-        def dfs(r, c):
-            print(f"{r},{c}")
-            if (
-                r < 0
-                or r == len(image)
-                or c < 0
-                or c == len(image[0]) or (r, c) in visit):
-                return
-            if image[r][c] != start_c:
-                return
-            visit.add((r, c))
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-        
-        dfs(sr, sc)
-        for r, c in visit:
+            for r, c in dir_arr:
+                new_r, new_c = c_r + r, c_c + c
+                if (
+                    new_r < 0
+                    or new_r == len(image)
+                    or new_c < 0
+                    or new_c == len(image[0])
+                    or (new_r, new_c) in visited
+                    or image[new_r][new_c] != start_color
+                ):
+                    continue
+                else:
+                    queue.append((new_r, new_c))
+            
+            visited.add((c_r, c_c))
+
+        print(visited)
+        for r,c in visited:
             image[r][c] = color
-
         return image
