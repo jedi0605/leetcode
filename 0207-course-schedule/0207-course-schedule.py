0b130eef3726169e0104eb5c178a0b894d21652e
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2024-11-21 16:55:52 -0500

    Time: 11 ms (17.83%), Space: 18.4 MB (41.55%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,10 +1,9 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
         preMap = defaultdict(list)
-        for cor,pre  in prerequisites:
-            preMap[cor].append(pre)
-        # print(preMap)
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
         visited = set()
 
         def dfs(cur):
@@ -12,14 +11,17 @@ class Solution:
                 return False
             if preMap[cur] == []:
                 return True
+
             visited.add(cur)
-            for pre in preMap[cur]:
-                if dfs(pre) == False:
+            for crs in preMap[cur]:
+                if dfs(crs) == False:
                     return False
             visited.remove(cur)
             preMap[cur] = []
             return True
+        
         for crs in range(numCourses):
             if dfs(crs) == False:
                 return False
         return True
+
