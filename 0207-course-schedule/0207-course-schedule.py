ee73af7b23791f5ed1883cfc536b9a2f2c84c5f6
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2024-11-22 19:38:17 -0500

    Time: 7 ms (48.28%), Space: 18.5 MB (41.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -3,25 +3,23 @@ class Solution:
         preMap = defaultdict(list)
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
-        
-        visited = set()
 
+        print(preMap)
+        visited = set()
         def dfs(cur):
             if cur in visited:
                 return False
             if preMap[cur] == []:
                 return True
-
             visited.add(cur)
-            for crs in preMap[cur]:
-                if dfs(crs) == False:
-                    return False
+            for num in preMap[cur]:
+                if dfs(num) == False:
+                    return False                    
             visited.remove(cur)
-            preMap[cur] = []
+            preMap[cur] = []        
             return True
         
-        for crs in range(numCourses):
-            if dfs(crs) == False:
-                return False
+        for n in range(numCourses):
+            if dfs(n) == False:
+                return False                    
         return True
-
