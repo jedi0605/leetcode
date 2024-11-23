343c657e02db9577939d76c93f675c418ea53b60
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2024-11-22 20:00:51 -0500

    Time: 11 ms (55.33%), Space: 12.8 MB (85.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,12 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        cnt = Counter(nums)
+        # cnt = Counter(nums)
+        # res = 0
+        # max_cnt = 0
+        # print(cnt)
+        # for num, c in cnt.items():
+        #     if c > max_cnt:
+        #         max_cnt = c
+        #         res = num
+        # return res
+
         res = 0
-        max_cnt = 0
-        print(cnt)
-        for num, c in cnt.items():
-            if c > max_cnt:
-                max_cnt = c
-                res = num
+        cnt = 0
+        for n in nums:
+            if cnt == 0:
+                res = n
+            if res == n:
+                cnt+=1
+            else:
+                cnt -=1
         return res
