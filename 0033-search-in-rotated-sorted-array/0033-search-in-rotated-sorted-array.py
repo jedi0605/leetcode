05fa79a7774315f56f5639ebcc9bdefdf7dbd218
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-02-07 12:33:50 -0500

    Time: 0 ms (100%), Space: 18.1 MB (58.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,35 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         def findPeak():
-            l,r = 0, len(nums)-1
-            if nums[0] < nums[-1] or len(nums)==1:
-                return r
-            while l<=r:
-                mid = (l+r) // 2
-                if nums[mid] > nums[mid+1]:
+            if len(nums) == 1:
+                return 0
+            if nums[0] < nums[-1]:
+                return len(nums) - 1
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] > nums[mid + 1]:
                     return mid
                 if nums[mid] >= nums[l]:
-                    l = mid +1
+                    l = mid + 1
                 else:
-                    r = mid -1
-            return 0
-        peak = findPeak()
-        print(peak)
-        l,r = 0 , len(nums) -1
-        if nums[l] <= target <= nums[peak]:
-            r = peak
+                    r = mid - 1
+            return r
+
+        peakIdx = findPeak()
+        print(peakIdx)
+        l, r = 0, len(nums) - 1
+        if nums[l] <= target <= nums[peakIdx]:
+            r = peakIdx
         else:
-            l = peak+1
+            l = peakIdx + 1
         print(f"{l},{r}")
-        while l<=r:
-            mid = (l+r) // 2
+        while l <= r:
+            mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
             if nums[mid] < target:
-                l = mid+1
+                l = mid + 1
             else:
-                r= mid -1
+                r = mid - 1
         return -1
-        
-        
-        
