ef795b4d822b30a19d648a92d1d12ce4f1769ee7
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-02-10 17:39:10 -0500

    Time: 0 ms (100%), Space: 18.1 MB (39.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,34 +1,36 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def findPeak():
+
+        def searhPeak():
             if len(nums) == 1:
                 return 0
             if nums[0] < nums[-1]:
-                return len(nums) - 1
+                return len(nums) - 1  # no rotation
             l, r = 0, len(nums) - 1
             while l <= r:
                 mid = (l + r) // 2
                 if nums[mid] > nums[mid + 1]:
                     return mid
-                if nums[mid] >= nums[l]:
+                if nums[l] <= nums[mid]:
                     l = mid + 1
                 else:
                     r = mid - 1
-            return r
+            return l
 
-        peakIdx = findPeak()
-        print(peakIdx)
+        peak = searhPeak()
+        print(peak)
         l, r = 0, len(nums) - 1
-        if nums[l] <= target <= nums[peakIdx]:
-            r = peakIdx
+        if nums[l] <= target <= nums[peak]:
+            r = peak
         else:
-            l = peakIdx + 1
+            l = peak + 1
+
         print(f"{l},{r}")
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] < target:
+            if nums[mid] <= target:
                 l = mid + 1
             else:
                 r = mid - 1
