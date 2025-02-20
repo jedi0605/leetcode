67cf70fc3e5b29ae3709082a876b17db4eff1210
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-02-20 12:55:21 -0500

    Time: 4 ms (21.22%), Space: 17.8 MB (69.31%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.py
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.py
@@ -1,26 +1,34 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        # BF, list all combinations??
         length = len(nums[0])
-        comb = []
-        tmp = []
-        # "1" , "0"
-        # i = 0
-        # tmp = [1]
-        def dfs(i):
-            if len(tmp) == length:
-                comb.append("".join(tmp[:]))
-                return
-            tmp.append("0")
-            dfs(i + 1)
-            tmp.pop()
-            tmp.append("1")
-            dfs(i + 1)
-            tmp.pop()
-
-        dfs(0)
         numSet = set(nums)
-        for s in comb:
-            if s not in numSet:
-                return s
-        
+        def generate(curr):
+            if len(curr) == length:
+                if curr not in numSet:
+                    return curr
+                return ""
+            add_zero = generate(curr+"0")
+            if add_zero:
+                return add_zero
+            return generate(curr+"1")
+        return generate("")
+        # # BF, list all combinations??
+        # length = len(nums[0])
+        # comb = []
+        # tmp = []
+        # def dfs(i):
+        #     if len(tmp) == length:
+        #         comb.append("".join(tmp[:]))
+        #         return
+        #     tmp.append("0")
+        #     dfs(i + 1)
+        #     tmp.pop()
+        #     tmp.append("1")
+        #     dfs(i + 1)
+        #     tmp.pop()
+
+        # dfs(0)
+        # numSet = set(nums)
+        # for s in comb:
+        #     if s not in numSet:
+        #         return s
