93c59f85bc9601dc4d246e838b30a4ce757cf513
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-02-20 23:37:12 -0500

    Time: 356 ms (47.67%), Space: 18.1 MB (21.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,18 +3,16 @@ class Solution:
         max_p = ""
         for i in range(len(s)):
             l, r = i, i
-            while l >= 0 and r < len(s):
-                subS = s[l : r + 1]
-                if subS == subS[::-1] and len(subS) >= len(max_p):
-                    max_p = subS
+            while l >= 0 and r < len(s) and s[l]==s[r]:
+                if(r-l+1) > len(max_p):
+                    max_p = s[l:r+1]                
                 l -= 1
                 r += 1
                 
             l,r = i,i+1
-            while l >= 0 and r < len(s):
-                subS = s[l : r + 1]
-                if subS == subS[::-1] and len(subS) >= len(max_p):
-                    max_p = subS
+            while l >= 0 and r < len(s)and s[l]==s[r]:
+                if(r-l+1) > len(max_p):
+                    max_p = s[l:r+1]                
                 l -= 1
                 r += 1
                
