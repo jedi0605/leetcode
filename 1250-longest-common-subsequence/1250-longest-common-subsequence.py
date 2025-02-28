b98461d905d992f82eb23bf150c5d8bf0cdc7c76
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-02-28 14:53:09 -0500

    Time: 463 ms (64.7%), Space: 42.5 MB (77.03%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -6,7 +6,7 @@ class Solution:
         for r in range(1, row):
             for c in range(1, col):
                 if text2[r - 1] == text1[c - 1]:
-                    dp[r][c] = max(dp[r - 1][c], dp[r][c - 1]) + 1
+                    dp[r][c] = dp[r - 1][c-1] + 1
                 else:
                     dp[r][c] = max(dp[r - 1][c], dp[r][c - 1])
         return dp[-1][-1]
