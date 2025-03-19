baf8d1187365e06147fa3ed17f260810280123b2
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-03-19 12:40:05 -0400

    Time: 148 ms (38.05%), Space: 21.6 MB (89.2%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -3,13 +3,12 @@ class Solution:
         res = 0
         r = 0
         while r < len(nums):
-            if nums[r] != 1 and r+2 < len(nums):
+            if nums[r] != 1 and r + 2 < len(nums):
                 # flipping
                 nums[r] = 1
-                nums[r+1] = 0 if nums[r+1]==1 else 1
-                nums[r+2] = 0 if nums[r+2]==1 else 1
-                res+=1
-            r +=1
+                nums[r + 1] = 0 if nums[r + 1] == 1 else 1
+                nums[r + 2] = 0 if nums[r + 2] == 1 else 1
+                res += 1
+            r += 1
 
-        return res if sum(nums) == len(nums) else -1
-        
+        return res if nums[-1] == 1 and nums[-2] == 1 else -1
