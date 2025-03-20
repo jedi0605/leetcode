8ea1db4bcadad8a1a09ec4f587158caa8a44eb2f
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-03-20 18:03:16 -0400

    Time: 200 ms (95.07%), Space: 93.4 MB (41.3%) - LeetHub

---
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.py
@@ -0,0 +1,46 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def union(self, x, y):
+        x_p, y_p = self.find(x), self.find(y)
+        if x_p == y_p:
+            return True
+        if self.rank[x_p] > self.rank[y_p]:
+            self.parent[y_p] = x_p
+            self.rank[x_p] += 1
+        else:
+            self.parent[x_p] = y_p
+            self.rank[y_p] += 1
+        return True
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+
+class Solution:
+    def minimumCost(
+        self, n: int, edges: List[List[int]], query: List[List[int]]
+    ) -> List[int]:
+        uf = UnionFind(n)
+        for a, b, edge in edges:
+            uf.union(a, b)
+        componet_cost = {}
+        for a, b, edge in edges:
+            root = uf.find(a)
+            if root not in componet_cost:
+                componet_cost[root] = edge
+            else:
+                componet_cost[root] &= edge
+        print(componet_cost)
+        res = []
+        for src, dst in query:
+            r1, r2 = uf.find(src), uf.find(dst)
+            if r1 != r2:
+                res.append(-1)
+            else:
+                res.append(componet_cost[r1])
+        return res
