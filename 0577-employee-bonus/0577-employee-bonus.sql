2fae87eb00b4455597e429e6bc7b79646878dd1c
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-03-24 18:33:40 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT name, SUM(Bonus.bonus) as bonus FROM Employee
+LEFT JOIN Bonus ON Employee.empId = Bonus.empId
+WHERE bonus is NULL or bonus <= 1000
+GROUP BY Employee.empId
