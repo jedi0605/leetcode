2286bc671fecc32088185ea5f7cfc9b3e2e769be
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-04-05 12:18:03 -0400

    Time: 75 ms (5.12%), Space: 23.2 MB (5.06%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,30 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def xor_total(arr: List[int]):
+            res = 0
+            for a in arr:
+                res = xor(a, res)
+            return res
+
+        sub_set = []
+        tmp = []
+
+        def backtracking(idx):
+            if idx == len(nums):
+                sub_set.append(tmp.copy())
+                return
+            if idx > len(nums):
+                return
+            # for i in range(idx, len(nums)):
+            tmp.append(nums[idx])
+            backtracking(idx + 1)
+            tmp.pop()
+            backtracking(idx + 1)
+            return
+
+        backtracking(0)
+        print(sub_set)
+        res = 0
+        for arr in sub_set:
+            res += xor_total(arr)
+        return res
