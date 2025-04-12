6147c209d3628cfb73301c5a2bbb4fd255451dee
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-04-12 14:01:11 -0400

    Time: 3253 ms (80.73%), Space: 17.9 MB (64.7%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,30 +1,33 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(row, col, idx):
+        visited = set()
+        direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        def dfs(r, c, idx):
             if (
-                row < 0
-                or col < 0
-                or row == len(board)
-                or col == len(board[0])
-                or board[row][col] == "."
-                or board[row][col] != word[idx]
+                r < 0
+                or r == len(board)
+                or c < 0
+                or c == len(board[0])
+                or board[r][c] != word[idx]
             ):
                 return False
             if idx == len(word) - 1:
                 return True
-            tmp = board[row][col]
-            board[row][col] = "."
+            tmp = board[r][c]
+            board[r][c] = "."
             found = (
-                dfs(row + 1, col, idx + 1)
-                or dfs(row - 1, col, idx + 1)
-                or dfs(row, col + 1, idx + 1)
-                or dfs(row, col - 1, idx + 1)
+                dfs(r + 1, c, idx + 1)
+                or dfs(r, c + 1, idx + 1)
+                or dfs(r - 1, c, idx + 1)
+                or dfs(r, c - 1, idx + 1)
             )
-            board[row][col] = tmp
+            board[r][c] = tmp
             return found
 
         for i in range(len(board)):
             for j in range(len(board[0])):
+                visited = set()
                 if board[i][j] == word[0] and dfs(i, j, 0):
                     return True
         return False
