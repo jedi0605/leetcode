8c57e716a913309c1ffb9b736005afa7d427eb9a
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-04-18 19:26:02 -0400

    Time: 0 ms (100%), Space: 17.6 MB (98.95%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -15,7 +15,6 @@ class Solution:
         maps[0].append(root)
         while q:
             node, col = q.popleft()
-            print(node.val)
             if node.left:
                 maps[col - 1].append(node.left)
                 q.append((node.left, col - 1))
@@ -23,10 +22,7 @@ class Solution:
                 maps[col + 1].append(node.right)
                 q.append((node.right, col + 1))
         res = []
-        max_col = max(maps)
-        min_col = min(maps)
-        for i in range(min_col, max_col + 1):
-            if len(maps[i]) != 0:
-                tmp = [i.val for i in maps[i]]
-                res.append(tmp)
+        for k in sorted(maps):
+            res.append([node.val for node in maps[k]])
         return res
+
