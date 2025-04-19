465ad0eb4d334a753c640fa78a8e2303cd73ea1c
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-04-19 19:35:29 -0400

    Time: 223 ms (59.82%), Space: 31.6 MB (53.7%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -1,11 +1,15 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        ans = 0
+        # brute force
         nums.sort()
+        res = 0
+
         for i in range(len(nums) - 1):
-            minReq = lower - nums[i]
-            maxReq = upper - nums[i]
-            low = bisect_left(nums, minReq, i+1)
-            high = bisect_right(nums, maxReq, i+1)
-            ans += high - low
-        return ans
+            # 0, 1, 4, 4, 5, 7
+            lo = lower - nums[i]
+            hi = upper - nums[i]
+            left_b = bisect_left(nums, lo,i+1)
+            right_b = bisect_right(nums, hi,i+1)
+            res += right_b - left_b
+
+        return res
