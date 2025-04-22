22ca4176fe4c634a6d2e84a7952eecdf6c2fb8e8
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-04-22 17:59:17 -0400

    Time: 79 ms (73.04%), Space: 32.4 MB (15.45%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
@@ -1,36 +1,12 @@
 class Solution:
     def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
-        def worked(mid):
-            start = mid
-            for diff in differences:
-                tmp = start + diff
-                if tmp < lower:
-                    return -1  # too small
-                if tmp > upper:
-                    return 1  # too big
-                start = tmp
+        org_arr = [0] * (len(differences)+1)
+        for i in range(len(differences)):
+            org_arr[i+1] = differences[i]+org_arr[i]
+        min_arr = min(org_arr)
+        max_arr = max(org_arr)
+        diff = max_arr - min_arr
+        range_diff = upper - lower
+        if diff > range_diff:
             return 0
-
-        left, right = lower, upper
-        while left <= right:
-            mid = (left + right) // 2
-            res = worked(mid)
-            if res == -1:
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        lowest = left
-        if lowest > upper:
-            return 0
-        left, right = lower, upper
-        while left <= right:
-            mid = (left + right) // 2
-            res = worked(mid)
-            if res == 1:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        highest = right
-        return highest - lowest + 1
+        return range_diff-diff +1
