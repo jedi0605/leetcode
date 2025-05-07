43526198fc9018e12b8f72cb93b461ffff4a999e
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-05-07 15:10:53 -0400

    Time: 101 ms (69.23%), Space: 28.7 MB (60.43%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -1,23 +1,20 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        maps = defaultdict(list) # node, nei
+        maps = defaultdict(list) # node -> prob,nei_node
         for i in range(len(edges)):
-            a,b = edges[i][0],edges[i][1]
-            maps[a].append( (b ,succProb[i]))
-            maps[b].append((a,succProb[i]))
+            a,b = edges[i][0], edges[i][1]
+            maps[a].append((succProb[i],b))
+            maps[b].append((succProb[i],a))
         max_heap = [(-1,start_node)]
-        max_prob = [0.0] * n
-        max_prob[start_node] = 1.0
-
+        visited = set()
         while max_heap:
             cur_prob, node = heappop(max_heap)
-            cur_prob = -1 * cur_prob
-            if node == end_node:
-                return cur_prob
-            for nei, prob in maps[node]:
-                new_prob = cur_prob * prob
-                if new_prob > max_prob[nei]:
-                    max_prob[nei] = new_prob
-                    heappush(max_heap, (-new_prob,nei))
+            cur_prob = cur_prob * -1
+            visited.add(node)
+            if node == end_node: return cur_prob
+            
+            for nei_prob,nei in maps[node]:
+                if nei not in visited:
+                    new_prob = nei_prob * cur_prob * -1
+                    heappush(max_heap, (new_prob,nei))
         return 0
-        
