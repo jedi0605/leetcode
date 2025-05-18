18b4be5412c6fe559c33e176504e6f54e3b62847
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-05-18 12:02:25 -0400

    Time: 1071 ms (39.88%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT name, SUM(Bonus.bonus) as bonus FROM Employee
-LEFT JOIN Bonus ON Employee.empId = Bonus.empId
-WHERE bonus is NULL or bonus < 1000
-GROUP BY Employee.empId
+SELECT name,bonus
+FROM Employee
+LEFT JOIN Bonus ON Employee.empID = Bonus.empId
+WHERE bonus < 1000 or bonus is NULL
