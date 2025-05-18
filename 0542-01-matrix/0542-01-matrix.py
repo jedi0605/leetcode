6e08b86bf50da584a8ad605a35c0cdd9f4887929
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-05-18 10:55:59 -0400

    Time: 177 ms (41.82%), Space: 19.6 MB (89.19%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,29 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        direc = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        visited = set()
-        res = []
+        rows,cols = len(mat),len(mat[0])
+        dir = [(0,1),(0,-1),(1,0),(-1,0)]
         queue = deque()
-        for i in range(len(mat)):
-            res.append([])
-            for j in range(len(mat[0])):
-                if mat[i][j] == 0:
-                    res[i].append(0)
-                    visited.add((i, j))
-                    queue.append([i, j])
-                else:
-                    res[i].append(float("inf"))
 
+        for i in range(rows):
+            for j in range(cols):
+                if mat[i][j] == 0:
+                    queue.append((i,j))
+                else:
+                    mat[i][j] = float('inf')
+        
         while queue:
-            for _ in range(len(queue)):
-                c_r, c_c = queue.popleft()
-                for x, y in direc:
-                    if (
-                        0 <= c_r + x < len(mat)
-                        and 0 <= c_c + y < len(mat[0])
-                        and (c_r + x, c_c + y) not in visited
-                    ):
-                        visited.add((c_r + x, c_c + y))
-                        res[c_r + x][c_c + y] = res[c_r][c_c] + 1
-                        queue.append([c_r + x, c_c + y])
-        return res
+            row, col = queue.popleft()
+
+            for dr,dc in dir:
+                new_r,new_c = row+dr,col +dc
+                if 0<=new_r<rows and 0<=new_c<cols and mat[new_r][new_c] > mat[row][col]:
+                    mat[new_r][new_c] = mat[row][col]+1
+                    queue.append((new_r,new_c))
+        return mat
