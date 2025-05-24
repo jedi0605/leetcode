6808a8562d82e9d015550e9db8b5c527d9f10c94
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-05-23 23:19:45 -0400

    Time: 75 ms (77.6%), Space: 27.4 MB (40.05%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -4,11 +4,10 @@ class Solution:
         cur_sum = 0
         for i in range(k):
             cur_sum += nums[i]
-        res = max(res,cur_sum / k)
+        res = max(res,cur_sum)
+        for i in range(k, len(nums)):
+            cur_sum -= nums[i - k]
+            cur_sum += nums[i]
+            res = max(res, cur_sum)
 
-        for i in range(k,len(nums)):            
-            cur_sum-= nums[i-k]
-            cur_sum+= nums[i]
-            res = max(res,cur_sum / k)
-
-        return res
+        return res / k
