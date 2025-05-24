37966cda99a1e31268a4c786bfc1fff1767d916c
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-05-23 23:16:46 -0400

    Time: 99 ms (30.85%), Space: 27.4 MB (78.12%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        res = float("-inf")
+        cur_sum = 0
+        for i in range(k):
+            cur_sum += nums[i]
+        res = max(res,cur_sum / k)
+
+        for i in range(k,len(nums)):            
+            cur_sum-= nums[i-k]
+            cur_sum+= nums[i]
+            res = max(res,cur_sum / k)
+
+        return res
