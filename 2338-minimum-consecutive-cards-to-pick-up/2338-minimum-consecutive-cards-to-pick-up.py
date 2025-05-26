c9944780820c96ae99eb548a61507304dece02ff
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-05-26 14:42:37 -0400

    Time: 67 ms (86.46%), Space: 38.2 MB (46.41%) - LeetHub

--- 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
+++ 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
@@ -1,23 +1,12 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        res = float('inf')
+        res = float('inf')        
+        maps = defaultdict(int) # nums / idx
 
-        
-        have_pair = False
-        maps = defaultdict(int) # nums / cnt
-
-        l = 0
-        # 3:1
-        for r in range(len(cards)):
-            num =cards[r] 
-            maps[num]+=1
-            if maps[num] >1:
-                have_pair = True
-                # res = min(res, r-l+1)
-            while have_pair:
-                res = min(res, r-l+1)
-                maps[cards[l]]-=1
-                if cards[l] == num:
-                    have_pair = False
-                l+=1
-        return res if res != float('inf') else -1
+        for idx, num in enumerate(cards):
+            if num in maps:
+                res = min(res,idx - maps[num] +1 )
+                maps[num] = idx
+            else:
+                maps[num] = idx
+        return res if res!=float('inf') else -1
