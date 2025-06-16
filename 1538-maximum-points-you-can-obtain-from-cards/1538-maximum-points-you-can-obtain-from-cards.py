a1b6cffa7ce94d25162131eee1b10a4b918e6fd1
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2025-06-15 21:22:26 -0400

    Time: 107 ms (5.24%), Space: 28.7 MB (6.04%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -1,13 +1,21 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        # from left?
-        print(cardPoints[:k])
-        left_sum = sum(cardPoints[:k])
-        right_sum = 0
-        max_sum = left_sum
+        if len(cardPoints) == k:
+            return sum(cardPoints)
+
+        init_arr = cardPoints[:k] # choose left k
+        init_sum = sum(init_arr)
+        max_sum = init_sum
+        m =  len(cardPoints)
         for i in range(k):
-            left_sum -= cardPoints[k-i-1]
-            right_sum += cardPoints[len(cardPoints)-i-1]
-            total = left_sum+right_sum
-            max_sum = max(max_sum,total)
+            # end of the cur arr
+            end_num = cardPoints[k - i -1]
+            print(end_num)
+            init_sum -= end_num
+
+            # add right of the k
+            right_num = cardPoints[m - i -1]
+            print(right_num)
+            init_sum += right_num
+            max_sum = max(max_sum,init_sum)
         return max_sum
