0c0814900608e2ac69038192f94eb4eb022eb338
Meng Chieh Lee (Jedi) <jedi2454@gmail.com>
2026-03-16 21:26:14 -0400

    Time: 286 ms (53.41%), Space: 42.2 MB (40.86%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.py
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.py
@@ -1,22 +1,21 @@
 class Solution:
     def minMirrorPairDistance(self, nums: List[int]) -> int:
 
-        def reverse(num) -> int:
-            strings = str(num)[::-1]
-            buildS = []
-            for i in range(len(strings)):
-                if strings[i] != "0":
-                    buildS = strings[i:]
-                    break
-            return int("".join(buildS))
+        # def reverse(num) -> int:
+        #     strings = str(num)[::-1]
+        #     buildS = []
+        #     for i in range(len(strings)):
+        #         if strings[i] != "0":
+        #             buildS = strings[i:]
+        #             break
+        #     return int("".join(buildS))
 
         dic = defaultdict(int)  # store reverse and max idx
         res = inf
         for idx, val in enumerate(nums):
-            if val in dic:
-                
+            if val in dic:                
                 res = min(res, idx - dic[val])
-            rInt = reverse(val)
+            rInt = int(str(val)[::-1])
             dic[rInt] = max(dic[rInt], idx)
         print(dic)
         return -1 if res == inf else res
